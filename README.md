29255619bf78e0c8eadea8e0e344181e1def50be
rubi <naiman.music.info@gmail.com>
2023-06-27 15:26:55 +0200

    textplorer - first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # textplorer
 A terminal-based file explorer written in Rust!
+
+If you try to open "enter" a file, you will instead open the file using the [helix editor](https://github.com/helix-editor/helix).
+If you don't have helix installed please change the text editor in the source code (because `textplorer` doesn't support command line arguments yet). 
+
+![](./imgs/showcase.png)
+
+# Usage
+1. Run `cargo build`
+2. Add the executable to your `PATH`
+3. Run it with `textplorer`!
---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use inquire::Select;
+use std::{
+    env,
+    fs::{self, metadata},
+    path::PathBuf,
+    process::Command,
+};
+
+fn get_absolute_path(path: &str) -> String {
+    let abs_path = fs::canonicalize(PathBuf::from(path));
+    let mut abs_path_string = abs_path.unwrap().as_path().to_string_lossy().into_owned();
+
+    // remove windows long path prefix (\\?\....) Ex: '\\?\C:\repos\'
+    if abs_path_string.starts_with(r"\\?\") {
+        abs_path_string = String::from(&abs_path_string[r"\\?\".len()..]);
+    }
+
+    abs_path_string
+}
+
+fn get_dir_entries(path: &str) -> Vec<String> {
+    // println!("{}", path);
+    let entries = fs::read_dir(path).unwrap();
+    let mut paths: Vec<String> = Vec::new();
+
+    paths.push("..".to_string());
+
+    for entry in entries {
+        let path_str = entry.unwrap().path().to_string_lossy().into_owned();
+        paths.push(path_str);
+    }
+
+    paths
+}
+
+#[warn(unused_assignments)]
+fn get_cwd() -> String {
+    env::current_dir()
+        .unwrap()
+        .as_path()
+        .to_string_lossy()
+        .into_owned()
+}
+
+fn clearscreen() {
+    // clear screen and position cursor at row 1 column 1
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+}
+
+fn main() {
+    let mut cwd = get_cwd();
+    let mut paths: Vec<String> = Vec::new();
+
+    loop {
+        // println!("{}", cwd);
+        paths = get_dir_entries(&cwd);
+        let ans = Select::new("", paths.clone()).prompt();
+
+        match ans {
+            Ok(choice) => {
+                if metadata(&choice).unwrap().is_dir() {
+                    // set current dir changes the cwd to the parent folder of the argument given
+                    cwd = get_absolute_path(&choice);
+                    let _ = env::set_current_dir(cwd.clone());
+                    // cwd = choice.clone();
+                } else {
+                    if cfg!(target_os = "windows") {
+                        let _ = Command::new("cmd")
+                            .args(["/C", "start", "hx", &choice])
+                            .spawn();
+                    } else {
+                        let _ = Command::new("sh").args(["-c", "hx", &choice]).spawn();
+                    };
+                };
+            }
+            Err(_) => {
+                clearscreen();
+                std::process::exit(0);
+            }
+        }
+    }
+}
